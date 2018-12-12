5ecb9de5da103a16c5d21336aa08e8d1f25d90d9
Kouch-Sato <redkouchred@gmail.com>
2018-12-12 21:41:46 +0900

    first component

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BodyText from './src/elements/BodyText';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Kota Shidara</Text>
+        <Text>Kouch Sato</Text>
         <Text>He is good</Text>
+        <BodyText>This is Child</BodyText>
       </View>
     );
   }
