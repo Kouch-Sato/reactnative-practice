69f7389fd3d6e62d248c89f248e103d3097067cc
Kouch-Sato <redkouchred@gmail.com>
2018-12-22 16:53:52 +0900

    create todoEdit screen

---
+++ src/screens/TodoEditScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class TodoEditScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.todoEditInput} multiline value="SpeechLinksのSEO対策"/>
+        <CircleButton style={styles.editButton}>
+          {'\uf00c'}
+        </CircleButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  todoEditInput: {
+    flex: 1,
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontSize: 20,
+  },
+});
+
+export default TodoEditScreen;
